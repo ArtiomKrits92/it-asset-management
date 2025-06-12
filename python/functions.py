a2836e043ae4e3a90020e73412c312fca19bd2d0
dcoacher <dcoacher@outlook.com>
2025-06-12 19:46:36 +0300

    Update functions.py

--- python/functions.py
+++ python/functions.py
@@ -13,7 +13,7 @@ def login_screen(): # Login Screen function
             print(f"✅ Success: You has been successfully logged in as `{username}`.")    # Success case - break the loop and login
             break
         else:
-            print("❌ Error: Incorrect username/password data has been provided. Please try again.\n")   # Incorrect data provided case, return to the loop start
+            print("❌ Error: Incorrect username/password data has been entered. Please try again.\n")   # Incorrect data provided case, return to the loop start
 
 def menu_handler(): # Menu Handler function
     print("\n1. Add New Item")
@@ -26,3 +26,34 @@ def menu_handler(): # Menu Handler function
     print("8. Sort Items by Name (A-Z)")
     print("9. Sort Items by Price (₪ - ₪₪₪)")
     print("0. Calculate Stock by Categories\n")
+
+def m1():   # Add New Items
+    print("Menu Option 1 has been choosen.\n")
+
+def m2():   # Delete Item
+    print("Menu Option 2 has been choosen.\n")
+
+def m3():   # Modify Item
+    print("Menu Option 3 has been choosen.\n")
+
+def m4():   # Assign User
+    print("Menu Option 4 has been choosen.\n")
+
+def m5():   # Add New User
+    print("Menu Option 5 has been choosen.\n")
+
+def m6():   # Show All Users
+    print("Menu Option 6 has been choosen.\n")
+
+def m7():   # Show All Items by the User
+    print("Menu Option 7 has been choosen.\n")
+
+def m8():   # Sort Items by Name (A-Z)
+    print("Menu Option 8 has been choosen.\n")
+
+def m9():   # Sort Items by Price (₪ - ₪₪₪)
+    print("Menu Option 9 has been choosen.\n")
+
+def m0():   # Calculate Stock by Categories
+    print("Menu Option 0 has been choosen.\n")
+
