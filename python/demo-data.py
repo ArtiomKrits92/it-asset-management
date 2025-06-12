6c3036b1cea81af787b1adf63bd441faef017ad9
Your Name <you@example.com>
2025-06-12 22:25:37 +0300

    Created demo-data Python file

---
+++ python/demo-data.py
@@ -0,0 +1,3 @@
+### This is a file which contains db pre-created values for project demonstration. 
+### Those values will be imported to the project.
+### New data added using project functions will be added/modified as well to the existing db with the pre-created and earlier added data from this file.
