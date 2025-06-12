c524e9ebb1b70ef9274dada7ae60fccde69013c2
dcoacher <dcoacher@outlook.com>
2025-06-12 19:46:21 +0300

    Update main.py

--- python/main.py
+++ python/main.py
@@ -1,42 +1,42 @@
 ### This is a Main file which will contain Welcome Screen and Main Menu features. 
 import functions # Importing functions module from functions.py file
 functions.welcome_screen() # Welcome Screen (will be displayed once and not included to the menu loop)
-functions.login_screen()    # Adding Login Screen feature using admin username and password credentials
+functions.login_screen()    # Adding Login Screen feature using admin username and functions.m()word credentials
 
 while True: # Start of Loop Menu
     functions.menu_handler()
-    user_choise = input("\nPlease choose menu option (q for exit):\n")
+    user_choise = input("\nPlease choose menu option (q for exit): ")
 
     match user_choise:  # Start of Menu Options, based on match cases
         case "1":
-            pass
+            functions.m1()
 
         case "2":
-            pass
+            functions.m2()
 
         case "3":
-            pass
+            functions.m3()
 
         case "4":
-            pass
+            functions.m4()
 
         case "5":
-            pass
+            functions.m5()
 
         case "6":
-            pass
+            functions.m6()
 
         case "7":
-            pass
+            functions.m7()
 
         case "8":
-            pass
+            functions.m8()
 
         case "9":
-            pass
+            functions.m9()
 
         case "0":
-            pass
+            functions.m0()
         
         case "q":   # Exit
             print("Thank you for using our service. See you later!\n")   # Print goodbye message to the user before quit the loop
@@ -45,5 +45,6 @@ while True: # Start of Loop Menu
         case _:
             print("❌ Error: Incorrect menu option has been choosen. Please try again.") # In case the user's chosen option does not exist
     # End of Menu Options, based on match cases
+
     input("Press Enter key in order to return to the Main Menu.") # Prompt the input message as a part of the loop for returning to the Main Menu
 # End of Loop Menu
