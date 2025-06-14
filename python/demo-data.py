0743810728aa6f7f5420e8c4a240d6b79e786ab8
Your Name <you@example.com>
2025-06-14 15:06:29 +0300

    Changed some files

--- python/demo-data.py
+++
@@ -1,7 +0,0 @@
-### This is a file which contains db pre-created values for project demonstration. 
-### Those values will be imported to the project.
-### New data added using project functions will be added/modified as well to the existing db with the pre-created and earlier added data from this file.
-db = [
-    {"id": "1", "user1": "Desmond"},
-    {"id": "2", "user2": "Artiom"},
-]
