211a58d39d53e271530feb9fba0b027e9eb50fa3
desmondcoacher <desmond.c@campus.technion.ac.il>
2025-07-07 16:57:13 +0300

    Updated specific website required module files

--- website/wsgi.py
+++ website/wsgi.py
@@ -2,6 +2,6 @@ import sys
 import logging
 
 logging.basicConfig(stream=sys.stderr)
-sys.path.insert(0, "/home/ubuntu/it_asset_app")
+sys.path.insert(0, "/var/www/it-asset-management")
 
 from app import application  # import your Flask app's WSGI callable
