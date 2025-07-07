cba9be5666ce03104aa56f8a0356343ff50e0b3f
desmondcoacher <desmond.c@campus.technion.ac.il>
2025-07-07 17:03:51 +0300

    Changes logic in wsgi.py file

--- website/wsgi.py
+++ website/wsgi.py
@@ -4,4 +4,4 @@ import logging
 logging.basicConfig(stream=sys.stderr)
 sys.path.insert(0, "/var/www/it-asset-management")
 
-from app import application  # import your Flask app's WSGI callable
+from app import app as application  # import your Flask app's WSGI callable
