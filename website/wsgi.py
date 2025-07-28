41e96f6c44cedd5b82a23bb2dae264d96de9a517
desmondcoacher <desmond.c@campus.technion.ac.il>
2025-07-28 13:17:42 +0300

    Removed not relevant files

--- website/wsgi.py
+++
@@ -1,7 +0,0 @@
-import sys
-import logging
-
-logging.basicConfig(stream=sys.stderr)
-sys.path.insert(0, "/var/www/it-asset-management")
-
-from app import app as application  # import your Flask app's WSGI callable
